5281ff02f67b393b2bb7b5c0b0995ca869f27f82
Daniel Maiochi <danimaiochi@gmail.com>
2021-07-16 09:56:45 +0100

    Adding test

--- ReliableDownloader.Tests/Tests.cs
+++ ReliableDownloader.Tests/Tests.cs
@@ -1,19 +1,25 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ReliableDownloader.Tests
 {
     [TestFixture]
     public class Tests
     {
+        private string _smallFileUrl = "https://upload.wikimedia.org/wikipedia/en/d/d0/Dogecoin_Logo.png";
+        private string _localDir = "D:/Projects/ReliableDownloader-main/";
+
         [SetUp]
         public void Setup()
         {
         }
-        
+
         [Test]
-        public void Test1()
+        public async Task DownloadFile_NonExistingSmallFileAndBigChunks_ShouldCreateFile()
         {
-            var fileDownloader = new FileDownloader();
+            var fileDownloader = new FileDownloader(100);
+            await fileDownloader.DownloadFile(_smallFileUrl, _localDir + "image.png", progress => Console.WriteLine($"progress: {progress.ProgressPercent}"));
             Assert.True(true);
         }
     }
